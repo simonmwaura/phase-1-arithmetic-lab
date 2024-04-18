c80613ce18ff6ec122bb6ff8acff740a9e649c04
simonmwaura <simonmwaura2090@gmail.com>
2024-04-18 13:09:13 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1=31
+let num2=2
+let multiply=num1*num2
+
+const random = Math.floor( Math.random() * 1000 ) + 1;
+
+ let num3 = 22
+ let num4 = 9
+ let mod=num3%num4
+
+ let max = (0,1,3,4,5,7,20)
+ max.math.max()
